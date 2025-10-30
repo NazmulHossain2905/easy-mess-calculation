6acdfebe004438396314f4d9bee665c66712ed8a
Nazmul Hossain <nazmulhossain2905@gmail.com>
2025-10-30 06:46:48 +0600

    initialize repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex-1 flex-col text-3xl font-bold text-red-600 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
